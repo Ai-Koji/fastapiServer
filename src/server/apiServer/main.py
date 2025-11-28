c3ee720e956703b8ce36ca51d009daf56447a472
artemism <artemism>
2025-11-29 00:45:46 +0700

    refactor: changed url

--- src/server/apiServer/main.py
+++ src/server/apiServer/main.py
@@ -10,11 +10,21 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://0.0.0.0:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Custom middleware to print globals
+@app.middleware("http")
+async def print_globals_middleware(request, call_next):
+    print("Globals information:")
+    from globals import devices
+    import json
+    print(json.dumps(devices, indent=4, ensure_ascii=False))
+    response = await call_next(request)
+    return response
 
 app.include_router(appRouter)
 app.include_router(authRouter)
